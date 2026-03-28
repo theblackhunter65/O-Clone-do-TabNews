3b68c210dc931ad4867f820c718ce92ca6ab3b3e
José Tiago <166430407+theblackhunter65@users.noreply.github.com>
2026-03-28 23:32:50 +0000

    Move .gitignore para a raiz e corrige estrutura

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7eb7045600353887"; }
+/******/ 		__webpack_require__.h = function() { return "86649314e283cc02"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
