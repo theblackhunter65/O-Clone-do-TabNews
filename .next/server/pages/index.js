c05579addd3fd1d2ab6bb4c872fef1442cb582a9
José Tiago <166430407+theblackhunter65@users.noreply.github.com>
2026-03-29 15:41:58 +0000

    adiciona scripts lint:check e lint:fix

--- .next/server/pages/index.js
+++ .next/server/pages/index.js
@@ -8,39 +8,41 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 (() => {
-var exports = {};
-exports.id = "pages/index";
-exports.ids = ["pages/index"];
-exports.modules = {
-
-/***/ "./pages/index.js":
-/*!************************!*\
+  var exports = {};
+  exports.id = "pages/index";
+  exports.ids = ["pages/index"];
+  exports.modules = {
+    /***/ "./pages/index.js":
+      /*!************************!*\
   !*** ./pages/index.js ***!
   \************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+      /***/ (
+        __unused_webpack_module,
+        __webpack_exports__,
+        __webpack_require__,
+      ) => {
+        eval(
+          '__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ "react/jsx-dev-runtime");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction Home() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {\n        children: /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)("div", {\n            children: [\n                /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)("h1", {\n                    children: "Sistema"\n                }, void 0, false, {\n                    fileName: "/workspaces/O-Clone-do-TabNews/pages/index.js",\n                    lineNumber: 5,\n                    columnNumber: 9\n                }, this),\n                /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)("p", {\n                    children: "feito por Jos\\xe9 Tiago"\n                }, void 0, false, {\n                    fileName: "/workspaces/O-Clone-do-TabNews/pages/index.js",\n                    lineNumber: 6,\n                    columnNumber: 9\n                }, this)\n            ]\n        }, void 0, true, {\n            fileName: "/workspaces/O-Clone-do-TabNews/pages/index.js",\n            lineNumber: 4,\n            columnNumber: 7\n        }, this)\n    }, void 0, false);\n}\nfunction Teste() {\n    console.log("teste");\n}\nfunction Teste2() {\n    console.log("Identa\\xe7\\xe3o errada");\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Home);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUEsU0FBU0EsT0FBTztJQUNkLHFCQUNFO2tCQUNFLDRFQUFDQzs7OEJBQ0MsOERBQUNDOzhCQUFHOzs7Ozs7OEJBQ0osOERBQUNDOzhCQUFFOzs7Ozs7Ozs7Ozs7O0FBSVg7QUFDQSxTQUFTQyxRQUFRO0lBQ2ZDLFFBQVFDLEdBQUcsQ0FBQztBQUNkO0FBQ0EsU0FBU0MsU0FBUztJQUNoQkYsUUFBUUMsR0FBRyxDQUFDO0FBQ2Q7QUFDQSxpRUFBZU4sSUFBSUEsRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL28tY2xvbmUtZG8tdGFibmV3cy8uL3BhZ2VzL2luZGV4LmpzP2JlZTciXSwic291cmNlc0NvbnRlbnQiOlsiZnVuY3Rpb24gSG9tZSgpIHtcbiAgcmV0dXJuIChcbiAgICA8PlxuICAgICAgPGRpdj5cbiAgICAgICAgPGgxPlNpc3RlbWE8L2gxPlxuICAgICAgICA8cD5mZWl0byBwb3IgSm9zw6kgVGlhZ288L3A+XG4gICAgICA8L2Rpdj5cbiAgICA8Lz5cbiAgKTtcbn1cbmZ1bmN0aW9uIFRlc3RlKCkge1xuICBjb25zb2xlLmxvZyhcInRlc3RlXCIpO1xufVxuZnVuY3Rpb24gVGVzdGUyKCkge1xuICBjb25zb2xlLmxvZyhcIklkZW50YcOnw6NvIGVycmFkYVwiKVxufVxuZXhwb3J0IGRlZmF1bHQgSG9tZTtcbiJdLCJuYW1lcyI6WyJIb21lIiwiZGl2IiwiaDEiLCJwIiwiVGVzdGUiLCJjb25zb2xlIiwibG9nIiwiVGVzdGUyIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/index.js\n',
+        );
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction Home() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n        children: \"Sistema\"\n    }, void 0, false, {\n        fileName: \"/workspaces/O-Clone-do-TabNews/pages/index.js\",\n        lineNumber: 2,\n        columnNumber: 9\n    }, this);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Home);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUEsU0FBU0EsT0FBTTtJQUNkLHFCQUFPLDhEQUFDQztrQkFBRzs7Ozs7O0FBQ1o7QUFDQSxpRUFBZUQsSUFBSUEsRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL28tY2xvbmUtZG8tdGFibmV3cy8uL3BhZ2VzL2luZGV4LmpzP2JlZTciXSwic291cmNlc0NvbnRlbnQiOlsiZnVuY3Rpb24gSG9tZSgpe1xuIHJldHVybiA8aDE+U2lzdGVtYTwvaDE+XG59XG5leHBvcnQgZGVmYXVsdCBIb21lO1xuIl0sIm5hbWVzIjpbIkhvbWUiLCJoMSJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./pages/index.js\n");
+        /***/
+      },
 
-/***/ }),
-
-/***/ "react/jsx-dev-runtime":
-/*!****************************************!*\
+    /***/ "react/jsx-dev-runtime":
+      /*!****************************************!*\
   !*** external "react/jsx-dev-runtime" ***!
   \****************************************/
-/***/ ((module) => {
+      /***/ (module) => {
+        module.exports = require("react/jsx-dev-runtime");
 
-module.exports = require("react/jsx-dev-runtime");
-
-/***/ })
-
-};
-;
-
-// load runtime
-var __webpack_require__ = require("../webpack-runtime.js");
-__webpack_require__.C(exports);
-var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = (__webpack_exec__("./pages/index.js"));
-module.exports = __webpack_exports__;
-
+        /***/
+      },
+  };
+  // load runtime
+  var __webpack_require__ = require("../webpack-runtime.js");
+  __webpack_require__.C(exports);
+  var __webpack_exec__ = (moduleId) =>
+    __webpack_require__((__webpack_require__.s = moduleId));
+  var __webpack_exports__ = __webpack_exec__("./pages/index.js");
+  module.exports = __webpack_exports__;
 })();
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
-function Home(){
- return <h1>Sistema</h1>
+function Home() {
+  return (
+    <>
+      <div>
+        <h1>Sistema</h1>
+        <p>feito por José Tiago</p>
+      </div>
+    </>
+  );
 }
+
 export default Home;
